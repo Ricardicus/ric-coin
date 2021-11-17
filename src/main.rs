34d1bc3f53cb74429aac9644ab7d3d3412af50d8
Rickard Hallerbäck <rickard.hallerback@gmail.com>
2021-11-17 22:04:42 +0100

    complaints

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-use std::io::{stdin,stdout,Write};
-
 mod keygenerator;
 use keygenerator::hash_string;
 mod blockchain;
 use blockchain::{Block, Printer, Transaction};
-
-fn prompt(prompt_text: &str) -> String {
-  let mut s = String::new();
-  print!("{}", prompt_text);
-  let _=stdout().flush();
-  stdin().read_line(&mut s).expect("Sorry, can't read that.");
-  if let Some('\n')=s.chars().next_back() {
-    s.pop();
-  }
-  if let Some('\r')=s.chars().next_back() {
-    s.pop();
-  }
-  return s;
-}
 
 fn main() {
   let t1: Transaction = Transaction::new(
@@ -25,7 +9,7 @@ fn main() {
   let mut transactions: Vec<Transaction> = Vec::new();
   transactions.push(t1);
   let mut block: Block = Block::new(
-      Vec::new(), 1337
+      transactions, 1337
   );
   let difficulty: usize = 4;
   println!("I got this block:\n{}", block.print());
